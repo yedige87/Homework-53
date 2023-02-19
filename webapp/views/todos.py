8b46d59048b4e14a02d9a9d1fe16232d38df5e31
yedige87 <yedabdygapparov@gmail.com>
2023-02-19 18:53:56 +0600

    add_view function; add.html

--- webapp/views/todos.py
+++ webapp/views/todos.py
@@ -3,3 +3,22 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from webapp.models import ToDo
+
+states = [{'id': '0', 'state': 'new', 'rus': 'Новая задача'}, {'id': '1', 'state': 'processing', 'rus': 'В процессе выполнения'},
+          {'id': '2', 'state': 'complited', 'rus': 'Задача завершена'}]
+
+
+def add_view(request: WSGIRequest):
+    if request.method == "GET":
+        return render(request, 'add.html', context={'states': states})
+    print(request.POST)
+    todo_data = {
+        'id': request.POST.get('id'),
+        'title': request.POST.get('title'),
+        'date_todo': request.POST.get('date_todo'),
+        'state': request.POST.get('state'),
+        'description': request.POST.get('description'),
+    }
+    # 'state_id': int(request.POST.get('state_id')),
+    todo = ToDo.objects.create(**todo_data)
+    return redirect(reverse('todo_view', kwargs={'pk': todo.pk}))
